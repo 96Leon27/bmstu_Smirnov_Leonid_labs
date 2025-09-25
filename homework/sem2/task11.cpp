da79b99997fad95aff1da8cc928e1d3e5e0b663c
96Leon27 <115868501+96Leon27@users.noreply.github.com>
2025-09-25 13:39:45 +0300

    Create task11.cpp

---
+++ homework/sem2/task11.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	// без комментариев
+	for (int i = 32; i <= 126; i++) {
+		cout << i << " - " << (char)i << endl;
+	}
+	
+
+	return 0;
+}

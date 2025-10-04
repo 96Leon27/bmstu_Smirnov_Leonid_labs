223e1a483d376ee0f909956fac435c1d2eeb375e
96Leon27 <115868501+96Leon27@users.noreply.github.com>
2025-10-04 09:27:25 +0300

    Create task3.cpp

---
+++ homework/sem3_geo/task3.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+// функция высчета дистанции между парой точек
+int d(double x1, double y1, double x2, double y2) {
+	return pow(pow(x1 - x2, 2) + pow(y1 - y2, 2), 0.5);
+}
+
+int main() {
+
+	// задание переменных
+	const int n = 3;
+
+	double arr_x[n] = {};
+	double arr_y[n] = {};
+
+	double x = 0;
+	double y = 0;
+	
+	
+	// цикл перебора вершин
+	for (int i = 0; i < n; i++) {
+		cout << "Введите координаты Х вершины " << i + 1 << ": ";
+		cin >> arr_x[i];
+
+		cout << "Введите координаты Y вершины " << i + 1 << ": ";
+		cin >> arr_y[i];
+	}
+
+	double Cx = (arr_x[0] + arr_x[1] + arr_x[2]) / 3;
+	double Cy = (arr_y[0] + arr_y[1] + arr_y[2]) / 3;
+
+	// вывод результатов
+	cout << "Центр масс Cx: " << Cx << endl << "Центр масс Cy: " << Cy << endl;
+
+	return 0;
+}
